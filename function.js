2edc5e2349570afc03dfa8fbdadf41937dd3e8a1
Maryam6713 <maryammalik6713@gmail.com>
2024-06-25 18:46:07 +0500

    initial commit

--- function.js
+++ function.js
@@ -1,7 +1,7 @@
 document.getElementById("checkButton").addEventListener("click", function() {
     var userInput = document.getElementById("userInput").value.toLowerCase();
     if (userInput === "friend" || userInput === "school" || userInput === "cricket" || userInput === "uniform") {
-        alert("Correct Answer");
+        alert( userInput + "  is correct answer");
     } else {
         alert("Incorrect Answer!");
     }
